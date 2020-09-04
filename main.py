2165e188ccb04f61e837fff4dc3fd42ce0381aed
Rahadian Permana <mrahadian.permana@gmail.com>
2020-09-05 04:41:18 +0700

    fix for loop bug

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import PyPDF2
 book = open('TheSecret.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
-
+print(pages)
 speaker = pyttsx3.init()
-for num in range(num, pages):
+for num in range(0, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
